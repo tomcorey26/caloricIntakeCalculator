d6d8806aacfce14a059ccfe1f4e6df6fa7ad409f
tomcorey26 <tomcorey26@gmail.com>
2019-02-13 19:04:09 -0500

    simplified handleChange and renderintake, fixed first submit bug

--- src/App.js
+++ src/App.js
@@ -1,76 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class NameForm extends React.Component {
+
+class NameForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gender: '',
-      age: '',
-      weight: '',
-      height: '',
-      actLevel: '',
+      gender: 'male',
+      age: '20',
+      weight: '150',
+      height: '67',
+      actLevel: 'sedentary',
       intake: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   renderIntake(gender,age,weight,height,actLevel){
-
+    console.log(this.state);
+    let modifiers = {
+      male: {
+        weight: 6.23,
+        height: 12.7,
+        age: 6.8,
+        basalMetabolic: 66
+      },
+      female: {
+        weight: 4.35,
+        height: 4.7,
+        age: 4.7,
+        basalMetabolic: 655
+      }
+    }
+  
     let levels = {
       sedentary: 1.2,
       light: 1.375,
       moderate: 1.55,
       heavy: 1.725
     }
-    if (gender === "male"){
-      let p1 = (weight * 6.23) + (height * 12.7);
-      let p2 = age * 6.8;
-      let p3 = p1-p2;
-      let basalMetabolic = p3 +66;
-      let caloricIntake = basalMetabolic * levels[actLevel];
-  
-      return caloricIntake
-    }
-    else {
-      let p1 = (weight * 4.35) + (height * 4.7);
-      let p2 = age * 4.7;
-      let p3 = p1-p2;
-      let basalMetabolic = p3 + 655;
-      let caloricIntake = basalMetabolic * levels[actLevel];
-  
-      return caloricIntake
-    }
+    
+    const modifier = modifiers[gender];
+    let p1 = (weight * modifier["weight"]) + (height * modifier["height"]);
+    let p2 = age * modifier["age"];
+    let p3 = p1-p2;
+    let basalMetabolic = p3 + modifier["basalMetabolic"];
+    let caloricIntake = basalMetabolic * levels[actLevel];
+
+    return caloricIntake
   }
 
   handleChange(event) {
-    if (event.target.name === 'gender'){
-      this.setState({gender: event.target.value});
-    }
-    else if (event.target.name === 'age'){
-      this.setState({age: event.target.value});
-    }
-    else if (event.target.name === 'weight'){
-      this.setState({weight: event.target.value});
-    }
-    else if (event.target.name === 'height'){
-      this.setState({height: event.target.value});
-    }
-    else if (event.target.value === 'sedentary'){
-      this.setState({actLevel: event.target.value});
-    }
-    else if (event.target.value === 'light'){
-      this.setState({actLevel: event.target.value});
-    }
-    else if (event.target.value === 'moderate'){
-      this.setState({actLevel: event.target.value});
-    }
-    else if (event.target.value === 'heavy'){
-      this.setState({actLevel: event.target.value});
-    }
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    console.log(value);
+    this.setState({
+      [name]: value
+    });
   }
   //have this return handle intake
   handleSubmit(event) {
+    console.log(this.state);
     let calories = this.renderIntake(this.state.gender,this.state.age,this.state.weight,this.state.height,this.state.actLevel);
     let round = Math.floor(calories);
     this.setState({intake: round});
@@ -79,42 +70,43 @@ class NameForm extends React.Component {
 
   render() {
     return (
-      <div class="theCalc">
+      <div className="calc">
         <form onSubmit={this.handleSubmit}>
-          <div class="theRow">
+          <div className="row">
             <label>
-              Male:
-              <input type="radio" value="male" onChange={this.handleChange} name="gender" />
-              {' '}
+              <span id="male-gender-input">
+                Male:
+                <input type="radio" value="male" name="gender" onChange={this.handleChange} name="gender" />
+              </span>
               Female:
-              <input type="radio" value="female" onChange={this.handleChange} name="gender"/>
+              <input type="radio" value="female" name="gender" onChange={this.handleChange} name="gender"/>
             </label>
           </div>
-          <div class="theRow">
+          <div className="row">
             <br/>
               <label>
-                Age: {' '}
+                <span className="title">Age: </span>
                 <input type="text" onChange={this.handleChange} name="age" />
               </label>
             <br/>
           </div>
-          <div class="theRow">
+          <div className="row">
               <label>
-                Weight: {' '}
+                <span className="title">Weight: </span> 
                 <input type="number" onChange={this.handleChange} name="weight" />
               </label>
             <br/>
           </div>
-          <div class="theRow">
+          <div className="row">
               <label>
-                Height: (in) {' '}
+                <span className="title">Height: (in) </span>
                 <input type="number" onChange={this.handleChange} name="height" />
               </label>
             <br />
           </div>
           <label>
-            Activity level: {' '}
-            <select value={this.state.actLevel} onChange={this.handleChange}>
+            <span className="title">Activity level:</span>
+            <select value={this.state.actLevel} onChange={this.handleChange} name="actLevel">
               <option value="sedentary">Sedentary (No Exercise)</option>
               <option value="light">Light (1-2 days)</option>
               <option value="moderate">Moderate (3-5)</option>
@@ -129,40 +121,16 @@ class NameForm extends React.Component {
           </h1>
         </form>
       </div>
-      
     );
   }
 }
 
-class Table extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-
-  }
-  render() {
-    return (
-      <div>
-        <NameForm/>
-      </div>
-    )
-  }
-}
-
-class App extends Component {
-  constructor() {
-    super()
-    this.table = <Table />;
-
-  }
-  render() {
+function App() {
     return (
       <div className="App">
-        {this.table}
+        <NameForm/>
       </div>
     );
-  }
 }
 
 
